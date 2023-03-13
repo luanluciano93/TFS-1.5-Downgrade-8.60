8bca57d28a387c50c47c3700d9a3522c0b6d5e7f
Luan Luciano <luanluciano@outlook.com>
2023-03-13 04:06:53 -0300

    [TFS] Magicwall shouldn't disappear on forced step-in
    [Jan 26, 2022] https://github.com/otland/forgottenserver/commit/e14ab10a971ea8dacc681cda6367c4ca9c1b9178

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Este downgrade não é uma distribuição de download e execução, monstros e feitiços provavelmente não estão 100% corretos.
 No entanto, você pode enviar uma solicitação pull.
 
-Este downgrade é até: Jan 25, 2022 - https://github.com/otland/forgottenserver/commit/d4a0aeef3bbca9ecf658ea50c55959ada6831ee3
+Este downgrade é até: Jan 26, 2022 - https://github.com/otland/forgottenserver/commit/e14ab10a971ea8dacc681cda6367c4ca9c1b9178
 
 ## Como Compilar:
 
--- src/combat.cpp
+++ src/combat.cpp
@@ -1473,6 +1473,25 @@ void AreaCombat::setupExtArea(const std::vector<uint32_t>& vec, uint32_t rows)
 
 void MagicField::onStepInField(Creature* creature)
 {
+	/* disabled on downgrade
+	//remove magic walls/wild growth
+	if (id == ITEM_MAGICWALL_SAFE || id == ITEM_WILDGROWTH_SAFE || isBlocking()) {
+		if (!creature->isInGhostMode()) {
+			g_game.internalRemoveItem(this, 1);
+		}
+
+		return;
+	}
+
+	//remove magic walls/wild growth (only nopvp tiles/world)
+	if (id == ITEM_MAGICWALL_NOPVP || id == ITEM_WILDGROWTH_NOPVP) {
+		if (g_game.getWorldType() == WORLD_TYPE_NO_PVP || getTile()->hasFlag(TILESTATE_NOPVPZONE)) {
+			g_game.internalRemoveItem(this, 1);
+		}
+		return;
+	}
+	*/
+
 	const ItemType& it = items[getID()];
 	if (it.conditionDamage) {
 		Condition* conditionCopy = it.conditionDamage->clone();
